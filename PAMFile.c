bfdf7cac713b107db98dfd964bb98def09eda3bd
LayoTito <layo.llts@gmail.com>
2024-06-13 14:01:55 -0300

    Update PAMFile.c

--- PAMFile.c
+++ PAMFile.c
@@ -18,7 +18,7 @@ typedef struct {
 
 } Question;
 
-char userPhone[];
+const char *userPhone;
 int i;
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
