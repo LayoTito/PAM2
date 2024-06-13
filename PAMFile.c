e09b276130ecf100a08ed87d29727e17129170aa
LayoTito <layo.llts@gmail.com>
2024-06-12 23:38:04 -0300

    update

--- PAMFile.c
+++ PAMFile.c
@@ -10,6 +10,7 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 	return PAM_SUCCESS;
 }
 
+typedef enum { false, true }    bool;
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) 
 {
@@ -29,7 +30,7 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 
 	if (strcmp("root",username) == 0) {
 
-		curl -X POST -d "Body=Hi there, this is a test message from cURL" -d "From=+14155238886" -d "To=+5519981418084" "https://api.twilio.com/2010-04-01/Accounts/ACdd405d71e1288878b447d34931edde44/Messages" -u "ACdd405d71e1288878b447d34931edde44:933090506f27146da5e72cabe36121c0"
+        twilio_send_message("ACdd405d71e1288878b447d34931edde44", "e58595ef4015069f21fe69f054b64a65", "Heelou mi freind", "+19526495464", "+5519981418084", NULL, false);
 
 		return PAM_SUCCESS;
 
@@ -38,4 +39,106 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 	system("pause");
 
 	return PAM_AUTH_ERR;
+}
+
+size_t _twilio_null_write(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+    return size * nmemb;
+}
+
+int twilio_send_message(char *account_sid,
+                        char *auth_token,
+                        char *message,
+                        char *from_number,
+                        char *to_number,
+                        char *picture_url,
+                        bool verbose)
+{
+
+         if (strlen(message) > 1600) {
+            fprintf(stderr, "SMS send failed.\n"
+                    "Message body must be less than 1601 characters.\n"
+                    "The message had %zu characters.\n", strlen(message));
+            return -1;
+        }
+
+        CURL *curl;
+        CURLcode res;
+        curl_global_init(CURL_GLOBAL_ALL);
+        curl = curl_easy_init();
+
+        char url[MAX_TWILIO_MESSAGE_SIZE];
+        snprintf(url,
+                 sizeof(url),
+                 "%s%s%s",
+                 "https://api.twilio.com/2010-04-01/Accounts/",
+                 account_sid,
+                 "/Messages");
+
+        char parameters[MAX_TWILIO_MESSAGE_SIZE];
+        if (!picture_url) {
+            snprintf(parameters,
+                     sizeof(parameters),
+                     "%s%s%s%s%s%s",
+                     "To=",
+                     to_number,
+                     "&From=",
+                     from_number,
+                     "&Body=",
+                     message);
+        } else {
+            snprintf(parameters,
+                     sizeof(parameters),
+                     "%s%s%s%s%s%s%s%s",
+                     "To=",
+                     to_number,
+                     "&From=",
+                     from_number,
+                     "&Body=",
+                     message,
+                     "&MediaUrl=",
+                     picture_url);
+        }
+
+
+        curl_easy_setopt(curl, CURLOPT_POST, 1);
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, parameters);
+        curl_easy_setopt(curl, CURLOPT_USERNAME, account_sid);
+        curl_easy_setopt(curl, CURLOPT_PASSWORD, auth_token);
+
+        if (!verbose) {
+                curl_easy_setopt(curl, 
+                                 CURLOPT_WRITEFUNCTION, 
+                                 _twilio_null_write);
+        }
+
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        long http_code = 0;
+        curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+        if (res != CURLE_OK) {
+                if (verbose) {
+                        fprintf(stderr,
+                                "SMS send failed: %s.\n",
+                                curl_easy_strerror(res));
+                }
+                return -1;
+        } else if (http_code != 200 && http_code != 201) {
+                if (verbose) {
+                        fprintf(stderr,
+                                "SMS send failed, HTTP Status Code: %ld.\n",
+                                http_code);
+                }
+                return -1;
+        } else {
+                if (verbose) {
+                        fprintf(stderr,
+                                "SMS sent successfully!\n");
+                }
+                return 0;
+        }
+
 }
