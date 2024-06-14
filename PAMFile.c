7b63ec1e36eecadec27bf485f0a3a852432f2047
LayoTito <layo.llts@gmail.com>
2024-06-13 23:57:52 -0300

    Update PAMFile.c

--- PAMFile.c
+++ PAMFile.c
@@ -69,10 +69,10 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
         printf("\nUm SMS foi enviado para confirmar seu login2");
 
         saveUserAccess(username);
-        setUseTime(username);
         printf("\n%s", userPhone);
         printf("\n%s", phoneNumber);
         sendMessage("ACdd405d71e1288878b447d34931edde44", "e58595ef4015069f21fe69f054b64a65", textMessage, "+19526495464", phoneNumber, false);
+        setUseTime(username);
 
     } else {
 
